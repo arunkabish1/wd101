872aece5060e099a17143c5231e47c0c7dac3545
arunkabish1 <141009715+arunkabish1@users.noreply.github.com>
2023-08-08 23:09:55 +0530

    index.js

--- index.js
+++ index.js
@@ -1,58 +1,74 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const registrationForm = document.getElementById('registrationForm');
-  const userTableBody = document.querySelector('#userTable tbody');
+const userForm = document.getElementById("user-form");
+const userEntries = retrieveEntries();
+const details = document.getElementById("user-entries");
 
-  // Load existing data from web storage (localStorage)
-  const userData = JSON.parse(localStorage.getItem('userData')) || [];
-  userData.forEach((user) => addUserToTable(user));
 
-  registrationForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const dob = document.getElementById('dob').value;
-    const acceptedTerms = document.getElementById('acceptedTerms').checked;
+function retrieveEntries() {
+  return JSON.parse(localStorage.getItem("UserEntries") || "[]");
+}
 
-    if (validateEmail(email) && validateDateOfBirth(dob) && acceptedTerms) {
-      const newUser = { name, email, password, dob, acceptedTerms };
-      addUserToTable(newUser);
-      saveUserData(newUser);
-      registrationForm.reset();
-    } else {
-      alert('Please provide valid Email, Date of Birth, and accept the terms.');
-    }
-  });
-
-  function validateEmail(email) {
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return emailRegex.test(email);
-  }
-
-  function validateDateOfBirth(dob) {
-    const currentDate = new Date();
-    const birthDate = new Date(dob);
-    const age = currentDate.getFullYear() - birthDate.getFullYear();
-    return age >= 18 && age <= 55;
-  }
-
-  function addUserToTable(user) {
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `
-      <td>${user.name}</td>
-      <td>${user.email}</td>
-      <td>${user.password}</td>
-      <td>${user.dob}</td>
-      <td>${user.acceptedTerms ? 'Yes' : 'No'}</td>
+function displayEntries() {
+  const tableEntries = userEntries
+    .map(({ name, email, password, dob, acceptTerms }) => `
+      <tr>
+        <td class="border px-4 py-2">${name}</td>
+        <td class="border px-4 py-2">${email}</td>
+        <td class="border px-4 py-2">${password}</td>
+        <td class="border px-4 py-2">${dob}</td>
+        <td class="border px-4 py-2">${acceptTerms}</td>
+      </tr>
+    `)
+    .join("\n");
+  const table = `
+    <table class="table-auto w-full">
+      <thead>
+        <tr>
+          <th class="px-4 py-2">Name</th>
+          <th class="px-4 py-2">Email</th>
+          <th class="px-4 py-2">Password</th>
+          <th class="px-4 py-2">Dob</th>
+          <th class="px-4 py-2">Accepted terms?</th>
+        </tr>
+        </thead>
+        <tbody>
+          ${tableEntries}
+        </tbody>
+      </table>
     `;
-    userTableBody.appendChild(newRow);
+  
+    details.innerHTML = table;
   }
-
-  function saveUserData(user) {
-    const userData = JSON.parse(localStorage.getItem('userData')) || [];
-    userData.push(user);
-    localStorage.setItem('userData', JSON.stringify(userData));
+  
+  function saveUserForm(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptTerms = document.getElementById("acceptTerms").checked;
+  
+    const currentYear = new Date().getFullYear();
+    const birthYear = dob.split("-");
+    const year = birthYear[0];
+    const age = currentYear - year;
+  
+  
+    if (age < 18 || age > 55) {
+      document.getElementById("dob").style.border = "1px solid red";
+      return alert("You must be between 18 and 55 years old to register");
+    }
+  
+    document.getElementById("dob").style.border = "none";
+  
+    const entry = { name, email, password, dob, acceptTerms };
+    userEntries.push(entry);
+  
+    localStorage.setItem("UserEntries", JSON.stringify(userEntries));
+    displayEntries();
+    userForm.reset();
   }
-});
-
+  
+  userForm.addEventListener("submit", saveUserForm);
+  displayEntries();
 
